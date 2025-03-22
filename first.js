e89923ea770d6decbe5c8174dfb5c9bd10d3c032
ankit-yadav90 <ankityadav73699@gmail.com>
2025-03-22 12:24:59 +0530

    number calculation

---
+++ first.js
@@ -0,0 +1,12 @@
+// //For Loop
+// for( let i = 1; i <= 5; i++) {
+//     console.log("Apna collage");
+// }
+
+//Calculate sum of 1 to n
+let sum = 0;
+let n = 100;
+for (let i = 1; i <= n; i++) {
+    sum = sum + i;
+}
+console.log("sum = ", sum);
